4b26a72aa606ad5a43071765066d58d782bee4d5
Leonardo115935 <157508358+Leonardo115935@users.noreply.github.com>
2024-02-01 09:54:32 -0600

    code

---
+++ week04/println.kt
@@ -0,0 +1,10 @@
+// HelloWorld/Task1.kt
+package helloWorldITSE1329
+
+// TODO
+
+fun main() {
+    println("Hello World!")
+    println("My name is Leonardo Hernandez")
+    println("I'm learn in ITSE-1329")
+}
