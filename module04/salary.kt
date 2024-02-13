c4340c001371a6f5b2ce859f4393a5579bf02445
Leonardo115935 <157508358+Leonardo115935@users.noreply.github.com>
2024-02-13 11:01:56 -0600

    Create salary.kt

---
+++ module04/salary.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var name: String = "Leonardo Hernandez"
+    var age: Int = 99
+    var annualPay: Double = 9999999.99
+    
+    println("My name is " + name + ", my age is" + age +".")
+}
