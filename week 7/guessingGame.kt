b896320119bb1942a8bc96f8206d0c539a2a33fe
Leonardo115935 <157508358+Leonardo115935@users.noreply.github.com>
2024-02-27 11:16:04 -0600

    code

--- week 7/guessingGame.kt
+++ week 7/guessingGame.kt
@@ -2,21 +2,19 @@
 //Angel Caro
 //Jordan Pham
 
-import kotlin.random.Random
-
 fun main() {
-    val randomNumber = Random.nextInt(1, 101) 
+    val randomNumber = (50)
     var guess: Int
     var attempts = 0
 
     println("Welcome to the Number Guessing Game!")
 
-    
+
     do {
         print("Guess the number (between 1 and 100): ")
         guess = readLine()?.toIntOrNull() ?: 0
 
-       
+
         when {
             guess > randomNumber -> println("Too high, try again.")
             guess < randomNumber -> println("Too low, try again.")
